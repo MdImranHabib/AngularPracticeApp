54776e7dd95e89fae03e3f27b14786b47e4caa13
MD IMRAN HABIB <mdimranhabib5@gmail.com>
2021-08-25 17:35:11 +0600

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlatListComponent } from './flats/flat-list/flat-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'flat-list', component: FlatListComponent },
+  { path: 'flat-create', component: FlatListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
